feae955f996a146941d577e60dc1126b36e60127
RoomeyRahman <roomeyrahman@gmail.com>
2023-12-11 23:01:30 +0600

    update

--- src/common/constants/message_patterns.constant.ts
+++ src/common/constants/message_patterns.constant.ts
@@ -1,4 +1,4 @@
-export enum PARTNERS {
+export enum PARTNER_MESSAGES {
     CREATE = 'partner-create',
     UPDATE = 'partner-update',
     FIND_ALL = 'partner-get-all',
@@ -6,8 +6,13 @@ export enum PARTNERS {
     DELETE = 'partner-delete'
 }
 
-export enum USERS {
+export enum USER_MESSAGES {
     CREATE = 'user-create',
+    AC_VERIFICATION = 'ac_verification',
+    GENERATE_TOKEN = 'generate_token',
+    GENERATE_PASSWORD_RESET_TOKEN = 'generate_password_reset_token',
+    FORGET_PASSWORD = 'forget_password',
+    RESET_PASSWORD = 'reset_password',
     UPDATE = 'user-update',
     FIND_ALL = 'user-get-all',
     FIND_ONE = 'user-get-one',
---
+++ src/common/index.ts
@@ -0,0 +1,6 @@
+export * from './constants';
+export * from './dto';
+export * from './interfaces';
+export * from './schemas';
+export * from './utils';
+export * from './filters';
