a20959096057df50b0e24a5ca8e47faeec42afe4
Md. Ataur Rahman Bhuiyan <roomeyrahman@gmail.com>
2023-12-21 03:30:30 +0000

    Hridoy

---
+++ src/common/constants/student.constant.ts
@@ -0,0 +1,46 @@
+export enum SPECIAL_NEEDS {
+    BLIND = 'Blind',
+    VISUALLY_IMPAIRED = 'Visually Impaired',
+    IMPAIRED_HEARING = 'Impaired Hearing',
+    SPEECH_IMPAIRMENT = 'Speech Impairment',
+    PHYSICALLY_CHALLENGED = 'Physically Challenged',
+    MENTALLY_CHALLENGED = 'Mentally Challenged',
+    ALBINISM = 'Albinism',
+    AUTISM = 'Autism',
+    OTHERS = 'Others',
+
+}
+
+export enum VACCINE_CARD {
+    BCG = 'BCG',
+    OPV_0 = 'OPV 0',
+    HBV_0 = 'HBV 0',
+    PENTAVALENT_1 = 'Pentavalent (DPT, Hep B,  Hib) 1',
+    PCV_1 = 'Pneumococcal Conjugate Vaccine(PCV) 1',
+    OPV_1 = 'OPV 1',
+    IPV_1 = 'IPV 1',
+    ROTAVIRUS_1 = 'Rotavirus Vaccine 1',
+    PENTAVALENT_2 = 'Pentavalent (DPT, Hep B,  Hib) 2',
+    PCV_2 = 'Pneumococcal Conjugate Vaccine(PCV) 2',
+    OPV_2 = 'OPV 2',
+    ROTAVIRUS_2 = 'Rotavirus Vaccine 2',
+    PENTAVALENT_3 = 'Pentavalent (DPT, Hep B,  Hib) 3',
+    PCV_3 = 'Pneumococcal Conjugate Vaccine(PCV) 3',
+    OPV_3 = 'OPV 3',
+    ROTAVIRUS_3 = 'Rotavirus Vaccine 3',
+    IPV_2 = 'IPV 2',
+    VITAMIN_A_1ST_DOSE = 'Vitamin A 1st Dose',
+    MEASLES_1ST_DOSE_MCV1 = 'Measles 1st Dose(MCV1)',
+    YELLOW_FEVER = 'Yellow Fever',
+    MENINGITIS_VACCINE_CSM = 'Meningitis Vaccine(CSM)',
+    VITAMIN_A_2ND_DOSE = 'Vitamin A 2nd Dose',
+    MEASLES_2ND_DOSE_MCV2 = 'Measles 2nd Dose(MCV2)',
+    HPV_VACCINE_1ST_DOSE = 'HPV Vaccine 1st Dose',
+    HPV_VACCINE_2ND_DOSE = 'HPV Vaccine 2nd Dose',
+}
+
+export enum RELATIONSHIP {
+    PARENTS = 'Parents',
+    FATHER = 'Father',
+    MOTHER = 'Mother'
+}
---
+++ src/common/constants/teacher.constant.ts
@@ -0,0 +1,148 @@
+export enum MARITAL_STATUS {
+    MARRIED = 'Married',
+    SINGLE = 'Single',
+    SEPARATED = 'Separated',
+    WIDOWED = 'Widowed',
+    DIVORCED = 'Divorced',
+}
+
+export enum EMPLOYER_TYPE {
+    FEDERAL = 'Federal',
+    NYSC = 'NYSC',
+    N_POWER = 'N-Power',
+    STATE = 'State',
+    COMMUNITY_PTA_SBMC = 'Community/PTA/SBMC',
+    VOLUNTEER = 'Volunteer',
+    PHILANTHROPIST = 'Philanthropist',
+
+}
+
+export enum APPOINMENT {
+    PERMANENT_PENSIONABLE = 'Permanent and Pensionable',
+    PERMANENT_NON_PENSIONABLE = 'Permanent and Non-pensionable',
+    CONTRACT = 'Contract',
+    PART_TIME = 'Part-Time',
+
+}
+
+export enum BLOOD {
+    A_POSITIVE = 'A+',
+    A_NEGATIVE = 'A-',
+    B_POSITIVE = 'B+',
+    B_NEGATIVE = 'B-',
+    AB_POSITIVE = 'AB+',
+    AB_NEGATIVE = 'AB-',
+    O_POSITIVE = 'O+',
+    O_NEGATIVE = 'O-',
+
+}
+
+export enum ACADEMIC_CERTIFICATION {
+    FSLC = 'F.S.L.C',
+    BECE_JSSCE = 'BECE/JSSCE',
+    WAEC = 'WAEC',
+    NECO = 'NECO',
+    NABTEB = 'NABTEB',
+    GCE = 'GCE',
+
+}
+
+export enum TERTIARY_CERTIFICATION {
+    NONE = 'None',
+    OND = 'OND',
+    HND = 'HND',
+    B_SC = 'B.Sc',
+    B_A = 'B.A',
+    B_TECH = 'B.Tech',
+    B_ENG = 'B.Eng',
+    M_SC = 'M.Sc',
+    M_A = 'M.A',
+    M_TECH = 'M.Tech',
+    M_ENG = 'M.Eng',
+    PHD = 'Ph.D',
+
+}
+
+export enum QUALIFICATION {
+    NONE = 'None',
+    NCE = 'NCE',
+    TCII = 'TCII',
+    ACE_ACIE = 'ACE/ACIE',
+    DIP_IN_EDU = 'Dip in EDU',
+    PGDE = 'PGDE',
+    B_ED = 'B.Ed',
+    BA_ED = 'BA.Ed',
+    BSC_ED = 'BSc.Ed',
+    M_ED = 'M.Ed',
+    MA_ED = 'MA.Ed',
+    MSC_ED = 'MSc.Ed',
+    PHD = 'Ph.D',
+
+}
+
+export enum SPECIALIZED_TEACHING_SKILLS {
+    SIGN_LANGUAGE_TEACHER = 'Standard Sign Language Teacher',
+    BRAILLE_TEACHING = 'Braile Teaching(For the Blind)',
+    AUTISM_STUDENT_SUPPORT = 'Autism Student Support',
+    DOWN_SYNDROME_SPECIALIST_TRAINING = 'Down Syndrome Specialist Training',
+    TEACHING_TRAINING_SUPPORT_FOR_DYSLEXIC_INDIVIDUALS = 'Teaching Training Support For Dyslexic Individuals',
+    SPECIAL_EDUCATION_TEACHER = 'Special Education Teacher',
+    OTHER = 'Other',
+
+}
+
+export enum COMPUTER_SKILLS {
+    COMPUTER_LITERACY = 'Basic Computer Literacy(Computer Appreciation)',
+    WORD_PROCESSING = 'Word Processing(Microsoft Word)',
+    ONLINE_LEARNING_PLATFORMS = 'Online Learning Platforms',
+    SOCIAL_MEDIA = 'Use of Social Media',
+    PRESENTATION_SOFTWARE = 'Presentation Software(Microsoft PowerPoint or Google Slide)',
+    CLASSROOM_MANAGEMENT_TOOLS = 'Classroom Management Tools',
+    EDUCATIONAL_SOFTWARE_AND_APPS = 'Educational Software and Apps',
+    INTERNET_NAVIGATION_AND_RESEARCH = 'Internet Navigation and Research(web Browsing)',
+    EMAIL_COMMUNICATION = 'Email Communication (Effective use of Email)',
+    INTERACTIVE_WHITEBOARD_TECHNOLOGY = 'Interactive Whiteboard Technology',
+    BASIC_GRAPHIC_DESIGN = 'Basic Graphic Design',
+    VIDEO_CONFERENCING_COLLABORATIVE_TOOLS = 'Video Conferencing/Collaborative Tools(Zoom,Teams, etc)',
+    DATA_ANALYSIS_AND_REPORTING = 'Data Analysis and Reporting(Spreadsheet,Queries and Programming Language)',
+
+}
+
+export enum LANGUAGE_SKILLS {
+    ENGLISH = 'English',
+    HAUSA = 'Hausa',
+    IGBO = 'Igbo',
+    YORUBA = 'Yoruba',
+    PIDGIN = 'Pidgin',
+    FRENCH = 'French',
+    SPANISH = 'Spanish',
+    OTHER_LANGUAGE = 'Other',
+
+}
+
+export enum TRAINING_CATEGORY {
+    NONE = 'None',
+    PEDAGOGY_AND_TEACHING_SKILLS = 'Pedagogy and Teaching Skills',
+    ASSESSMENT_AND_EVALUATION = 'Assessment and Evaluation',
+    CURRICULUM_AND_LESSON_PLANNING = 'Curriculum and Lesson Planning',
+    PERSONAL_DEVELOPMENT = 'Personal Development',
+    LEADERSHIP_AND_MANAGEMENT_SKILLS = 'Leadership and Management Skills',
+    SUBJECT_KNOWLEDGE = 'Subject Knowledge',
+    EDUCATION_TECHNOLOGY = 'Education Technology',
+    PROFESSIONAL_ETHICS_AND_LEGAL_STANDARDS = 'Professional Ethics and Legal Standards',
+    COMMUNICATION_AND_INTERPERSONAL_SKILLS = 'Communication and Interpersonal Skills',
+    CULTURAL_COMPETENCY_AND_INCLUSIVENESS = 'Cultural Competency and Inclusiveness',
+    CONFLICT_RESOLUTION_AND_MEDIATION = 'Conflict Resolution and Mediation',
+    PARENT_AND_COMMUNITY_ENGAGEMENT = 'Parent and Community Engagement',
+    CAREER_AND_COLLEGE_READINESS = 'Career and College Readiness',
+    ENVIRONMENTAL_EDUCATION = 'Environmental Education',
+    LITERACY_DEVELOPMENT = 'Literacy Development',
+    SOCIAL_AND_EMOTIONAL_LEARNING = 'Social and Emotional Learning',
+
+
+}
+
+export enum PROFESSIONAL_CERTIFICATE {
+    YES = 'Yes',
+    NO = 'No',
+}
